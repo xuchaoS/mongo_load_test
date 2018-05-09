5c8fc9935fb5da7833b9dab6296732a0552456e8
xuchaoS <xuchaoshang@gmail.com>
2018-05-09 01:30:21 -0400

    init

---
+++ mongo_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- encoding:utf-8 -*-
+"""
+File Name: mongo_test
+Author: shangxc
+Created Time: 2018/5/9 上午12:54
+"""
+
+
+if __name__ == '__main__':
+    pass
---
+++ setting.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- encoding:utf-8 -*-
+"""
+File Name: setting
+Author: shangxc
+Created Time: 2018/5/9 上午12:55
+"""
+ip = '127.0.0.1'
+port = 27017
+db_name = 'db_name'
+table_name = 'table_name'
+source_dir = 'dir'
+
+concurrent_num = 10
+delay = 1
+
+if __name__ == '__main__':
+    pass
